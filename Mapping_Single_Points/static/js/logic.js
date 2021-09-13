8747a9dc31520f55c03a34abe01025d7ffda3422
Adenine <30560532+jisellejones@users.noreply.github.com>
2021-09-12 17:11:13 -0700

    adjusted file structure and added circle marker to map

--- Mapping_Single_Points/static/js/logic.js
+++ Mapping_Single_Points/static/js/logic.js
@@ -2,7 +2,7 @@
 console.log("working");
 
 // One way - Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 4);
+let map = L.map('mapid').setView([34.0522, -118.2437], 14);
 
 // Another way - Create the map object with a center and zoom level. Let's you add
 // titles and such
@@ -13,8 +13,19 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 //     zoom: 4
 //   });
 
+// Add a marker for LA, California
+// let marker = L.marker([34.0522, -118.2437]).addTo(map);
+
+// Add a circle to the map
+L.circleMarker([34.0522, -118.2437], {
+    color: 'black',
+    fillColor: '#ffffa1',
+    fillOpacity: 0.5,
+    radius: 300
+}).addTo(map);
+
 // We create the tile layer that will be the background of our map.
-let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     accessToken: API_KEY
--- Simple_Map/static/js/logic.js
+++ Simple_Map/static/js/logic.js
@@ -13,6 +13,9 @@ let map = L.map('mapid').setView([40.7, -94.5], 4);
 //     zoom: 4
 //   });
 
+// Add a marker for LA, California
+let marker = L.marker([34.0522, -118.2437]);
+
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
